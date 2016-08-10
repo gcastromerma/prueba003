8ed7141430bd0a2d7c0cdab614e73929eb1ec8a3
gcastromerma <glorycast.23@hotmail.com>
2016-08-09 23:22:21 -0400

    ejemplo
    cvbcvb

---
+++ EmpleadoBO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using capa_dac;
+
+namespace capa_dao
+{
+  public class EmpleadoBO
+    {   
+
+      public static EmpleadoEntity obtenerEmpleadoPorUsuClave(string usu, string clave)
+      {
+          return EmpleadoDAL.obtenerEmpleadoPorUsuClave("sp_buscarEmpleadoPorUsuClave", usu, clave);
+      }
+
+      public static EmpleadoEntity insertaEmpleado_entity(EmpleadoEntity emp)
+      {
+          return EmpleadoDAL.insertaEmpleado("sp_insertaEmpleado", emp);
+      }
+      
+    }
+}
